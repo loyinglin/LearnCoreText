dd0e2a0e8a46d8ebed6952fa9b7688e3fcca8e82
loyinglin <loying@foxmail.com>
2018-10-24 11:14:03 +0800

    Initial Commit

---
+++ LearnCoreText/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  LearnCoreText
+//
+//  Created by loyinglin on 2018/10/24.
+//  Copyright © 2018 loyinglin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
---
+++ LearnCoreText/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LearnCoreText
+//
+//  Created by loyinglin on 2018/10/24.
+//  Copyright © 2018 loyinglin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
