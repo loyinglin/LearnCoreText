b57d7a9d824b0ccec3536991390bfda44bf11797
loyinglin <loying@foxmail.com>
2019-03-25 14:50:26 +0800

    增加滑页的页数控制。

--- PageEffect/LearnCoreText/LearnCoreText/FirstViewController.m
+++ PageEffect/LearnCoreText/LearnCoreText/FirstViewController.m
@@ -37,7 +37,7 @@
     SSSlideViewController *vc = [[SSSlideViewController alloc] init];
     vc.delegate = self;
     [self addChildViewController:vc];
-    [vc setInitVC:[self getRandomVC]];
+    [vc setInitVC:[self getRandomVCWithIndex:5]];
     [self.view addSubview:vc.view];
     
 }
@@ -51,11 +51,27 @@
 }
 
 - (UIViewController *)slideViewControllerGetLastVC:(SSSlideViewController *)scrollVC {
-    return [self getRandomVC];
+    UIViewController *ret;
+    UIViewController *vc = [scrollVC.viewControllers firstObject];
+    if (vc) {
+        NSInteger index = vc.view.tag;
+        if (index > 0) {
+            ret = [self getRandomVCWithIndex:index - 1];
+        }
+    }
+    return ret;
 }
 
 - (UIViewController *)slideViewControllerGetNextVC:(SSSlideViewController *)scrollVC {
-    return [self getRandomVC];
+    UIViewController *ret;
+    UIViewController *vc = [scrollVC.viewControllers lastObject];
+    if (vc) {
+        NSInteger index = vc.view.tag;
+        if (index < 10) {
+            ret = [self getRandomVCWithIndex:index + 1];
+        }
+    }
+    return ret;
 }
 
 - (void)slideViewController:(SSSlideViewController *)scrollVC willTransitionToViewControllers:(UIViewController *)pendingViewController {
@@ -66,14 +82,13 @@
     NSLog(@"info, transitionCompleted:%d", completed);
 }
 
-- (UIViewController *)getRandomVC {
+- (UIViewController *)getRandomVCWithIndex:(NSInteger)index {
     UIViewController *vc = [[UIViewController alloc] init];
     vc.view.backgroundColor = [UIColor colorWithRed:arc4random_uniform(255) / 255.0
                                               green:arc4random_uniform(255) / 255.0
                                                blue:arc4random_uniform(255) / 255.0
                                               alpha:1];
-    static NSInteger index;
-    ++index;
+    vc.view.tag = index;
     UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(100, 100, 100, 100)];
     label.text = [NSString stringWithFormat:@"页数%ld", (long)index];
     label.textColor = [UIColor blackColor];
