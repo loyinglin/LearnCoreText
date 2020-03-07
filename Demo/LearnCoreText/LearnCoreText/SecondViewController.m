011e93ed6676a575efca6ea0eb6c5cec183a8835
loyinglin <loying@foxmail.com>
2020-03-07 21:01:25 +0800

    测试方法。

--- Demo/LearnCoreText/LearnCoreText/SecondViewController.m
+++ Demo/LearnCoreText/LearnCoreText/SecondViewController.m
@@ -10,6 +10,7 @@
 
 @interface SecondViewController ()
 
+
 @end
 
 @implementation SecondViewController
@@ -17,6 +18,20 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSString *pathStr = [[NSBundle mainBundle] pathForResource:@"test" ofType:@"data"];
+    NSString *htmlStr = [NSString stringWithContentsOfURL:[NSURL fileURLWithPath:pathStr] encoding:NSUTF8StringEncoding error:nil];
+    
+    NSLog(@"load string:%@", htmlStr);
+    NSData *htmlData = [NSData dataWithBytes:htmlStr.UTF8String length:[htmlStr lengthOfBytesUsingEncoding:NSUTF8StringEncoding]];
+
+    NSDictionary *dic = @{NSDocumentTypeDocumentAttribute:NSHTMLTextDocumentType,
+                          NSCharacterEncodingDocumentAttribute:[NSNumber numberWithInt:NSUTF8StringEncoding]};
+
+    NSDate *date = [NSDate date];
+    NSAttributedString *attrStr = [[NSAttributedString alloc] initWithData:htmlData options:dic documentAttributes:nil error:nil];
+    
+    NSLog(@"transform time:%f", [[NSDate date] timeIntervalSinceDate:date]);
 }
 
 
